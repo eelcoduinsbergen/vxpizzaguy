6605731d61261eb0827e84ac25eb8a777fe6d25c
Eelco Duinsbergen <eduinsbergen@gmail.com>
2022-11-20 02:27:49 +0100

    conversation loop

--- src/VX.PizzaGuy/Program.cs
+++ src/VX.PizzaGuy/Program.cs
@@ -1,60 +1,13 @@
-﻿using Microsoft.CognitiveServices.Speech;
-using Microsoft.CognitiveServices.Speech.Audio;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VX.PizzaGuy.SpeechEngine;
 
 namespace VX.PizzaGuy
 {
-
-
     public class Program
     {
         async static Task Main(string[] args)
         {
-            var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
-            speechConfig.SpeechRecognitionLanguage = "nl-NL";
-
-            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
-            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
-            using var speechSynthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
-
-            var result = await speechSynthesizer.SpeakTextAsync("Hallo, wie is daar?");
-            
-            Console.WriteLine("Speak into your microphone.");
-            
-            var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
-            OutputSpeechRecognitionResult(speechRecognitionResult);
-        }
-
-        static string speechKey = "7c6a7fe1723e40b09b16b64e35d83bb0";
-        static string speechRegion = "northeurope";
-        
-
-        static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
-        {
-            switch (speechRecognitionResult.Reason)
-            {
-                case ResultReason.RecognizedSpeech:
-                    Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
-                    break;
-                case ResultReason.NoMatch:
-                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-                    break;
-                case ResultReason.Canceled:
-                    var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
-                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-                    if (cancellation.Reason == CancellationReason.Error)
-                    {
-                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
-                    }
-                    break;
-            }
+            var engine = new ChatBot();
+            await engine.Start();
         }
     }
 }
